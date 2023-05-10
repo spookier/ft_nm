f149c98b25186b7961b94592b3ac6a22326b6139
spookier <costinalexandru777@gmail.com>
2023-05-10 20:15:14 +0200

    fixed argc parsing

--- nm/src/parsing/parsing.c
+++ nm/src/parsing/parsing.c
@@ -18,15 +18,14 @@ int argc_parsing(int argc, char **argv)
 	else
 	{
 		i = 1;
-		flag = 0;
 		while(i < argc)
 		{
-			fd = open(argv[1], O_RDONLY);
+			flag = 0;
+			fd = open(argv[i], O_RDONLY);
 			if (fd == -1)
 			{
 				printf("nm: '%s': No such file\n", argv[i]);
 				flag = -1;
-				//return (-1);
 			}
 			if(flag != -1)
 			{
